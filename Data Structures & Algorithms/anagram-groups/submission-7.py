09efb79e32245441bbae7f7b13b33ec16d3e556e
Mahum Naveed <nmahum216@gmail.com>
2026-08-27 12:43:11 +0500

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.py
@@ -0,0 +1,18 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        hashMap = {}
+        for i in strs:
+            sorted_key = "".join(sorted(i))
+            if sorted_key not in hashMap:
+                hashMap[sorted_key] = []
+            hashMap[sorted_key].append(i)
+        return list(hashMap.values())
+             
+        # res = defaultdict(list)
+        # for s in strs:
+        #     count = [0] * 26
+        #     for c in s:
+        #         count[ord(c)-ord("a")] += 1
+        #     res [tuple(count)].append(s)
+        # return list(res.values())
+            
